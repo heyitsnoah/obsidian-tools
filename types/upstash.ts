05ae70219369a59294bbf17cfc5a6b987f5a8a7c
Noah Brier <nbrier@gmail.com>
2024-07-21 13:41:59 -0400

    chore: Add new route for scraping URLs and processing them

--- types/upstash.ts
+++ types/upstash.ts
@@ -1,5 +1,11 @@
 // Map your routes to message types
-export type RouteMessageMap = {}
+export type RouteMessageMap = {
+  '/api/summarize/urls/scrape': {
+    urlQueueKey: string
+    urlProcessedKey: string
+    urlBodiesKey: string
+  }
+}
 
 // Your UpstashRoute type remains the same
 export type UpstashRoute = keyof RouteMessageMap
