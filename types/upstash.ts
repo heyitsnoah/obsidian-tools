5c3ace82a10d564d4c00db2790f3cb99ab062842
Noah Brier <nbrier@gmail.com>
2024-07-31 22:44:10 -0400

    New routes

--- types/upstash.ts
+++ types/upstash.ts
@@ -1,10 +1,24 @@
+import { QueueKeys } from '@/utils/redis-queue'
+
+import { RecentDiff, RecentFile } from './files'
+
 // Map your routes to message types
 export type RouteMessageMap = {
   '/api/summarize/urls/scrape': {
-    urlQueueKey: string
-    urlProcessedKey: string
-    urlBodiesKey: string
+    url: string
+    keys: QueueKeys
   }
+  '/api/notes/summarize': {
+    note: RecentFile
+    keys: QueueKeys
+  }
+
+  '/api/notes/diffs/summarize': {
+    diff: RecentDiff
+    keys: QueueKeys
+  }
+
+  '/api/summarize/daily': string
 }
 
 // Your UpstashRoute type remains the same
