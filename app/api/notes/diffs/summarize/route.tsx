a8f2b838d607f34e5e0660b24352ca6b9105267b
Noah Brier <nbrier@gmail.com>
2024-08-01 15:52:28 -0400

    feat: Integrate anthropic AI for generating note summaries
    This commit integrates the anthropic AI library to generate note summaries in the '/api/notes/diffs/summarize' route. It uses the 'claude-3-5-sonnet-20240620' model to create a summary based on the provided diff. The generated summary is stored in Redis and associated with the corresponding note filename. This enhancement improves the note summarization feature and enhances the user experience.

--- app/api/notes/diffs/summarize/route.tsx
+++ app/api/notes/diffs/summarize/route.tsx
@@ -1,15 +1,34 @@
+import { TextBlock } from '@anthropic-ai/sdk/resources/messages.mjs'
 import { NextRequest } from 'next/server'
 
+import { getDiffSummarizationPrompt } from '@/prompts/notes/note-summary-user'
 import { RouteMessageMap } from '@/types/upstash'
+import { anthropic } from '@/utils/ai'
 import { redis } from '@/utils/redis'
 import { verifyUpstashSignature } from '@/utils/upstash'
 
 export async function POST(req: NextRequest) {
+  console.log('/api/notes/diffs/summarize')
+
   const body: RouteMessageMap['/api/notes/diffs/summarize'] =
     await verifyUpstashSignature(req)
-  console.log('/api/notes/diffs/summarize')
+  const response = await anthropic.messages.create({
+    max_tokens: 1000,
+    model: 'claude-3-5-sonnet-20240620',
+
+    messages: [
+      {
+        role: 'user',
+        content: getDiffSummarizationPrompt(body.diff.diff),
+      },
+    ],
+  })
+  if (!response) {
+    return new Response('No content found in response', { status: 500 })
+  }
+  const responseContent = (response.content[0] as TextBlock).text
   await redis.hset(body.keys.notesKey, {
-    [body.diff.filename]: body.diff.diff,
+    [body.diff.filename]: responseContent.trim(),
   })
   await redis.expire(body.keys.notesKey, 86400) // Set TTL for 24 hours
   return new Response('ok', { status: 200 })
