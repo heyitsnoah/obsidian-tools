1606544196354d865096f9fb890ba59141f96487
Noah Brier <nbrier@gmail.com>
2024-07-31 22:42:56 -0400

    chore: Update daily summary generator to use user-specific information
    This commit updates the daily summary generator to use user-specific information, such as the user's name and bio, instead of hardcoding Noah Brier's information. By incorporating the user's name and expertise, the generated daily summary will be more personalized and relevant. This change improves the functionality and customization of the daily summary generator.

--- app/api/summarize/daily/route.tsx
+++ app/api/summarize/daily/route.tsx
@@ -1,16 +1,14 @@
-import { TextBlock } from '@anthropic-ai/sdk/resources/messages.mjs'
 import dayjs from 'dayjs'
 import getUrls from 'get-urls'
 import { NextRequest } from 'next/server'
+import { z } from 'zod'
 
-import { getDailySummarySystemPrompt } from '@/prompts/summarize/daily-summary-user'
 import { RecentDiff, RecentFile } from '@/types/files'
-import { UrlBodies } from '@/types/urls'
-import { anthropic, openai } from '@/utils/ai'
-import { createOrUpdateFile, octokit } from '@/utils/github'
-import { redis } from '@/utils/redis'
-import { publishToUpstash } from '@/utils/upstash'
-import { getUrlKeys, storeUrls } from '@/utils/urls'
+import { RouteMessageMap } from '@/types/upstash'
+import { openai } from '@/utils/ai'
+import { octokit } from '@/utils/github'
+import { getQueueKeys } from '@/utils/redis-queue'
+import { publishToUpstash, verifyUpstashSignature } from '@/utils/upstash'
 export const maxDuration = 300
 export const dynamic = 'force-dynamic'
 const EXCLUDED_TERMS: string[] = []
@@ -23,6 +21,10 @@ if (process.env.WEEKLY_SUMMARY_NAME) {
 if (process.env.MONTHLY_SUMMARY_NAME) {
   EXCLUDED_TERMS.push(process.env.MONTHLY_SUMMARY_NAME)
 }
+
+const UsefulUrls = z.object({
+  usefulUrls: z.array(z.string()),
+})
 
 async function getRecentFiles(
   owner: string,
@@ -131,6 +133,74 @@ async function getRecentFiles(
   }
 }
 
+export async function POST(req: NextRequest) {
+  const body: RouteMessageMap['/api/summarize/daily'] =
+    await verifyUpstashSignature(req)
+  console.log('/api/summarize/daily')
+  console.log(body)
+  // *******************************************
+  // todo:
+  // 1. get all the files from the last 24 hours
+  // 2. update the prompt to take all teh file summaries
+  // 3. get rid of the action items
+  // 4. clean up the extra code
+  // *******************************************
+
+  // const response = await anthropic.messages.create({
+  //   messages: [
+  //     { role: 'user', content: getDailySummarySystemPrompt(recentFiles) },
+  //   ],
+  //   model: 'claude-3-5-sonnet-20240620',
+  //   max_tokens: 4000,
+  // })
+
+  // if (!response) {
+  //   return new Response('No content found in response', { status: 500 })
+  // }
+  // let responseContent = (response.content[0] as TextBlock).text
+  // const filename = `${process.env.DAILY_SUMMARY_NAME} ${dayjs().format(
+  //   'YYYY-MM-DD'
+  // )}${process.env.NODE_ENV === 'development' && `-DEV`}.md`
+  // const urlBodies: UrlBodies | null = await redis.hgetall(urlKeys.urlBodiesKey)
+  // if (urlBodies) {
+  //   const insertUrls = (
+  //     responseContent: string,
+  //     urlBodies: UrlBodies
+  //   ): string => {
+  //     const urlsList = Object.entries(urlBodies)
+  //       .map(([url, content]) => {
+  //         if (content) {
+  //           const { title, summary } = content
+  //           return `- [${title}](${url})${summary ? `\n    - ${summary}` : ''}`
+  //         }
+  //         return ''
+  //       })
+  //       .join('\n')
+
+  //     const actionItemsIndex = responseContent.indexOf('\n\n## Action Items')
+
+  //     if (actionItemsIndex === -1) {
+  //       // If "## Action Items" is not found, just append the URL list at the end
+  //       return `${responseContent}\n\n## Urls\n${urlsList}`
+  //     }
+
+  //     const beforeActionItems = responseContent.slice(0, actionItemsIndex)
+  //     const afterActionItems = responseContent.slice(actionItemsIndex)
+
+  //     return `${beforeActionItems}\n\n## Urls\n${urlsList}${afterActionItems}`
+  //   }
+  //   responseContent = insertUrls(responseContent, urlBodies)
+  // }
+  // console.log(responseContent)
+  // await createOrUpdateFile({
+  //   filename,
+  //   content: responseContent,
+  //   path: process.env.DAILY_SUMMARY_FOLDER,
+  //   inbox: true,
+  // })
+  return new Response('ok', { status: 200 })
+}
+
 export async function GET(req: NextRequest) {
   if (
     req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}` &&
@@ -143,104 +213,78 @@ export async function GET(req: NextRequest) {
 
   const recentFiles = await getRecentFiles(owner, repo)
   // return new Response('ok', { status: 200 })
-  const urlKeys = getUrlKeys()
-  console.log(urlKeys)
-  console.log(process.env.UPSTASH_REDIS_REST_URL)
-  const exists = await redis.exists(urlKeys.urlBodiesKey)
-  console.log(exists)
-  // return new Response('ok', { status: 200 })
-  if (!exists) {
-    console.log('running URLs')
-    const urls: string[] = []
-    recentFiles.files.map((file) => {
-      getUrls(file.body).forEach((url) => {
-        urls.push(url)
-      })
-    })
-    recentFiles.diffs.map((diff) => {
-      getUrls(diff.diff).forEach((url) => {
-        urls.push(url)
-      })
-    })
-    const openaiResponse = await openai.chat.completions.create({
-      model: 'gpt-4o-mini',
-      messages: [
-        {
-          role: 'system',
-          content:
-            'You will be given an array of URLs. Your job is to return the urls that represent valuable content, not the ones that are generic (like google.com, yahoo.com, nytimes.com, cnn.com, etc.), redirects, generic, shortened, and otherwise not useful. Return urls in this JSON format: {usefulUrls: string[]}',
-        },
-        {
-          role: 'user',
-          content: `URLs: ${JSON.stringify(urls)}}\nUseful URLs:`,
-        },
-      ],
-      response_format: { type: 'json_object' },
-    })
-    if (!openaiResponse.choices[0].message.content) {
-      return new Response('No content found in response', { status: 500 })
-    }
-    const parsed = JSON.parse(openaiResponse.choices[0].message.content)
-    await storeUrls(urlKeys.urlQueueKey, parsed.usefulUrls)
-    await publishToUpstash('/api/summarize/urls/scrape', {
-      urlQueueKey: urlKeys.urlQueueKey,
-      urlBodiesKey: urlKeys.urlBodiesKey,
-      urlProcessedKey: urlKeys.urlProcessedKey,
-    })
-    return new Response('processing urls', { status: 200 })
-  }
-  console.log('already has urls')
-  const response = await anthropic.messages.create({
-    messages: [
-      { role: 'user', content: getDailySummarySystemPrompt(recentFiles) },
-    ],
-    model: 'claude-3-5-sonnet-20240620',
-    max_tokens: 4000,
-  })
+  const queue = 'daily-note-queue'
+  const keys = getQueueKeys(queue)
 
-  if (!response) {
+  console.log('running URLs')
+  const urls: string[] = []
+  recentFiles.files.map((file) => {
+    getUrls(file.body).forEach((url) => {
+      urls.push(url)
+    })
+  })
+  recentFiles.diffs.map((diff) => {
+    getUrls(diff.diff).forEach((url) => {
+      urls.push(url)
+    })
+  })
+  const openaiResponse = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: [
+      {
+        role: 'system',
+        content:
+          'You will be given an array of URLs. Your job is to return the urls that represent valuable content, not the ones that are generic (like google.com, yahoo.com, nytimes.com, cnn.com, etc.), redirects, generic, shortened, and otherwise not useful. Return urls in this JSON format: {usefulUrls: string[]}',
+      },
+      {
+        role: 'user',
+        content: `URLs: ${JSON.stringify(urls)}}\nUseful URLs:`,
+      },
+    ],
+    response_format: { type: 'json_object' },
+  })
+  if (!openaiResponse.choices[0].message.content) {
     return new Response('No content found in response', { status: 500 })
   }
-  let responseContent = (response.content[0] as TextBlock).text
-  const filename = `${process.env.DAILY_SUMMARY_NAME} ${dayjs().format(
-    'YYYY-MM-DD'
-  )}.md`
-  const urlBodies: UrlBodies | null = await redis.hgetall(urlKeys.urlBodiesKey)
-  if (urlBodies) {
-    const insertUrls = (
-      responseContent: string,
-      urlBodies: UrlBodies
-    ): string => {
-      const urlsList = Object.entries(urlBodies)
-        .map(([url, content]) => {
-          if (content) {
-            const { title, summary } = content
-            return `- [${title}](${url})${summary ? `\n    - ${summary}` : ''}`
-          }
-          return ''
-        })
-        .join('\n')
-
-      const actionItemsIndex = responseContent.indexOf('\n\n## Action Items')
-
-      if (actionItemsIndex === -1) {
-        // If "## Action Items" is not found, just append the URL list at the end
-        return `${responseContent}\n\n## Urls\n${urlsList}`
-      }
-
-      const beforeActionItems = responseContent.slice(0, actionItemsIndex)
-      const afterActionItems = responseContent.slice(actionItemsIndex)
-
-      return `${beforeActionItems}\n\n## Urls\n${urlsList}${afterActionItems}`
-    }
-    responseContent = insertUrls(responseContent, urlBodies)
+  let parsed
+  try {
+    parsed = UsefulUrls.parse(
+      JSON.parse(openaiResponse.choices[0].message.content)
+    )
+  } catch (error) {
+    console.error('Error parsing response:', error)
+    return new Response('Error parsing response', { status: 500 })
   }
-  console.log(responseContent)
-  await createOrUpdateFile({
-    filename,
-    content: responseContent,
-    path: process.env.DAILY_SUMMARY_FOLDER,
-    inbox: true,
+
+  for (const file of recentFiles.files) {
+    await publishToUpstash(
+      '/api/notes/summarize',
+      { note: file, keys },
+      {
+        queue,
+      }
+    )
+  }
+  for (const diff of recentFiles.diffs) {
+    await publishToUpstash(
+      '/api/notes/diffs/summarize',
+      { diff, keys },
+      {
+        queue,
+      }
+    )
+  }
+  for (const url of parsed.usefulUrls) {
+    await publishToUpstash(
+      '/api/summarize/urls/scrape',
+      { url, keys },
+      {
+        queue,
+      }
+    )
+  }
+  await publishToUpstash('/api/summarize/daily', keys.notesKey, {
+    queue,
   })
-  return new Response('created file', { status: 200 })
+  return new Response('ok', { status: 200 })
 }
